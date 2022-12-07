689f369a73c27601ae8a0154c97e4a17f351de8a
ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2022-12-07 21:03:01 +0300

    perf: add edit user and post

--- components/admin/App.tsx
+++ components/admin/App.tsx
@@ -1,15 +1,21 @@
 import jsonServerProvider from 'ra-data-json-server';
-import { Admin, Resource } from 'react-admin';
+import { Admin, EditGuesser, Resource } from 'react-admin';
 import { CommentList } from './comments/List';
 import { PostList } from './posts/List';
+import { UserEdit } from './users/Edit';
 import { UserList } from './users/List';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} recordRepresentation="name" />
-    <Resource name="posts" list={PostList} />
+    <Resource
+      name="users"
+      list={UserList}
+      recordRepresentation="name"
+      edit={EditGuesser}
+    />
+    <Resource name="posts" list={PostList} edit={UserEdit} />
     <Resource name="comments" list={CommentList} />
   </Admin>
 );
